26981f9f384e54d460e5283cfd5a44542296fa14
bsp456 <bhabani.panda2005@gmail.com>
2026-08-29 00:20:36 +0530

    Caluclator Done

---
+++ Calculator/script.js
@@ -0,0 +1,40 @@
+let string = "";
+let buttons = document.querySelectorAll("button");
+Array.from(buttons).forEach((button) => {
+  button.addEventListener("click", (e) => {
+    
+    if (e.target.innerHTML == "=") {
+      
+        try{
+            string = eval(string);
+            document.querySelector("input").value = string;
+        }
+
+        catch(error) {
+            document.querySelector("input").value = "Error";
+            string = "";
+      
+        }
+     
+    } 
+
+    else if(e.target.innerHTML == "X") {
+      string = string.slice(0, -1);
+      document.querySelector("input").value = string;
+        
+    }
+
+    else if(e.target.innerHTML == "C") {
+        console.log(e.target);
+      string = "";
+      document.querySelector("input").value = string;
+        
+    }
+    
+    else {
+     
+      string = string + e.target.innerHTML;
+      document.querySelector("input").value = string;
+    }
+  });
+});
