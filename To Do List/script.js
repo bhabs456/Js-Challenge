12c9b378b36794deb7577f5c1f6363612b2c3a82
Bhabani S Panda <bhabani.panda2005@gmail.com>
2026-08-31 02:40:59 +0530

    To-Do List

--- To Do List/script.js
+++ To Do List/script.js
@@ -1,51 +1,40 @@
-const taskInput = document.getElementById("taskInput");
-const taskList = document.getElementById("taskList");
+const inputBox = document.getElementById('input-box');
+const listContainer = document.getElementById('list-container');
 
-function addTask() {
-  if (taskInput.value.trim() === "") {
-    alert("Write Something.");
-    return;
-  }
+function addTask(){
+    if(inputBox.value === ''){    // if input blank 
+        alert("Blank, Write Something.")
+    }
+    else{
+        let li = document.createElement("li");   // Make a element li
+        li.innerHTML = inputBox.value;  // store the data of input box in li html
+        listContainer.appendChild(li); // this to show the text in list task
 
-    let li = document.createElement("li");
-
-    li.innerHTML = taskInput.value;
-
-    let deleteBtn = document.createElement("button");
-
-    deleteBtn.textContent = "Delete";
-
-    deleteBtn.className = "delete-btn";
-
-    li.appendChild(deleteBtn);
-
-    taskList.appendChild(li);
+        let span = document.createElement("span");  // Create a element span
+        span.innerHTML = "\u00d7";  // add a unicode character cross
+        li.appendChild(span);  // this to show cross with the task
+    }
+    inputBox.value = ""; // this is to make input empty after adding.
     saveData();
-    taskInput.value = "";
-    
 }
 
-taskList.addEventListener("click", function(e){
-    if(e.target.classList.contains("delete-btn")){
-        e.target.parentElement.remove()
-        saveData();
+listContainer.addEventListener("click" , function(e){   // check clicks in the listContainer
+    if(e.target.tagName === "LI"){                      // if click on Li 
+        e.target.classList.toggle("checked");   
+        saveData();        // toggle checked feature and un toggle also
     }
-})
-
-taskInput.addEventListener("keydown", function(e) {
-
-    if (e.key === "Enter") {
-        addTask();
+    else if(e.target.tagName === "SPAN"){               // if click on span  
+        e.target.parentElement.remove();
+        saveData();                // remove parent element li (means delete the task)
     }
+}, false);
 
-});
-
-function saveData(){
-    localStorage.setItem("data", taskList.innerHTML);
+function saveData(){   // to store the changes when refreshed website
+    localStorage.setItem("data", listContainer.innerHTML);  // stores website data in list container in the name of data.
 }
 
-function showData() {
-    taskList.innerHTML = localStorage.getItem("data") || "";
+function showData(){
+    listContainer.innerHTML = localStorage.getItem("data");   // it shows data in website when refreshed in the name "data"
 }
 
-showData();
+showData();  // shows data
